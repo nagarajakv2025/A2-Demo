a1ffa6babd4fa8550da077ad1fe06fae97dee72a
user <user@DESKTOP-IKHEAKC>
2025-03-29 14:15:34 +0530

    Second commit

---
+++ src/test/java/selenium/Demo/Demo1.java
@@ -0,0 +1,17 @@
+package selenium.Demo;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+		@Test
+
+		public void method()
+
+		{
+			System.out.println("Good Morning");
+		}
+
+}
+	
+	
